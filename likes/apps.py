7d7387b72984828b89e72ce14924c4fe941a1d94
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-04-17 18:20:12 +0430

    new model is created

--- likes/apps.py
+++ likes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RestprojectConfig(AppConfig):
+class LikesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restProject'
+    name = 'likes'
