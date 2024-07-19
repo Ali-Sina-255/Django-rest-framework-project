98adbd891c20118b0ad738b3f7ffb54c08b27f9f
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-07-19 17:26:41 +0430

    secure sensitive info

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k3**n2o!mwkthfljriisc!f*^92ra8)z7$wel4$+(_r!ns0s=n'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -68,10 +69,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rest-store-project',
-        'USER': "postgres",
-        "PASSWORD": "open allah.",
-        "HOST": "localhost"
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST')
     }
 }
 
